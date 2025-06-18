9f8d1d92066d5ece2d4a22a43995a30b65221cbd
raihankhanmalek <raihankhanmalek@gmail.com>
2025-06-18 17:15:38 +0530

    changed settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -30,9 +30,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'eftq=^y3#8)kgr0psjp(f0w62f#_nbekde^k_x@fxezv(fcl3d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://portfolio-lnu0.onrender.com']
 
 
 # Application definition
@@ -131,7 +132,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_email_password'
+EMAIL_HOST_USER = 'raihankhanmalek@gmail.com'
+EMAIL_HOST_PASSWORD = 'Raihan@786'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
